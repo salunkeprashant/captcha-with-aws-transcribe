bc88b37447335a9d9f5724fca6eee4421d87f758
SANJUAN\prashant.salunke <prashant.salunke@avalara.com>
2022-08-25 14:46:43 +0530

    Initial implementation of captcha solver with aws transcribe

---
+++ gulpfile.js
@@ -0,0 +1,253 @@
+const path = require('path');
+const {exec} = require('child_process');
+const {lstatSync, readdirSync, readFileSync, writeFileSync} = require('fs');
+
+const {series, parallel, src, dest} = require('gulp');
+const babel = require('gulp-babel');
+const postcss = require('gulp-postcss');
+const gulpif = require('gulp-if');
+const jsonMerge = require('gulp-merge-json');
+const jsonmin = require('gulp-jsonmin');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const {ensureDirSync, readJsonSync} = require('fs-extra');
+const sharp = require('sharp');
+const CryptoJS = require('crypto-js');
+
+const targetEnv = process.env.TARGET_ENV || 'firefox';
+const isProduction = process.env.NODE_ENV === 'production';
+const distDir = path.join('dist', targetEnv);
+
+function clean() {
+  return del([distDir]);
+}
+
+function jsWebpack(done) {
+  exec('webpack-cli --display-error-details --bail --colors', function (
+    err,
+    stdout,
+    stderr
+  ) {
+    console.log(stdout);
+    console.log(stderr);
+    done(err);
+  });
+}
+
+function jsBabel() {
+  return src(['src/content/**/*.js'], {base: '.'})
+    .pipe(babel())
+    .pipe(dest(distDir));
+}
+
+const js = parallel(jsWebpack, jsBabel);
+
+function html() {
+  return src('src/**/*.html', {base: '.'})
+    .pipe(gulpif(isProduction, htmlmin({collapseWhitespace: true})))
+    .pipe(dest(distDir));
+}
+
+function css() {
+  return src(['src/solve/*.css'], {
+    base: '.'
+  })
+    .pipe(postcss())
+    .pipe(dest(distDir));
+}
+
+async function images(done) {
+  ensureDirSync(path.join(distDir, 'src/icons/app'));
+  const appIconSvg = readFileSync('src/icons/app/icon.svg');
+  const appIconSizes = [16, 19, 24, 32, 38, 48, 64, 96, 128];
+  for (const size of appIconSizes) {
+    await sharp(appIconSvg, {density: (72 * size) / 24})
+      .resize(size)
+      .toFile(path.join(distDir, `src/icons/app/icon-${size}.png`));
+  }
+  // Chrome Web Store does not correctly display optimized icons
+  if (isProduction && targetEnv !== 'chrome') {
+    await new Promise(resolve => {
+      src(path.join(distDir, 'src/icons/app/*.png'), {base: '.'})
+        .pipe(imagemin())
+        .pipe(dest('.'))
+        .on('error', done)
+        .on('finish', resolve);
+    });
+  }
+
+  await new Promise(resolve => {
+    src('node_modules/ext-contribute/src/assets/*.@(jpg|png|svg)')
+      .pipe(gulpif(isProduction, imagemin()))
+      .pipe(dest(path.join(distDir, 'src/contribute/assets')))
+      .on('error', done)
+      .on('finish', resolve);
+  });
+}
+
+async function fonts(done) {
+  await new Promise(resolve => {
+    src('src/fonts/roboto.css', {base: '.'})
+      .pipe(postcss())
+      .pipe(dest(distDir))
+      .on('error', done)
+      .on('finish', resolve);
+  });
+
+  await new Promise(resolve => {
+    src(
+      'node_modules/fontsource-roboto/files/roboto-latin-@(400|500|700)-normal.woff2'
+    )
+      .pipe(dest(path.join(distDir, 'src/fonts/files')))
+      .on('error', done)
+      .on('finish', resolve);
+  });
+}
+
+async function locale(done) {
+  const localesRootDir = path.join(__dirname, 'src/_locales');
+  const localeDirs = readdirSync(localesRootDir).filter(function (file) {
+    return lstatSync(path.join(localesRootDir, file)).isDirectory();
+  });
+  for (const localeDir of localeDirs) {
+    const localePath = path.join(localesRootDir, localeDir);
+    await new Promise(resolve => {
+      src(
+        [
+          path.join(localePath, 'messages.json'),
+          path.join(localePath, `messages-${targetEnv}.json`)
+        ],
+        {allowEmpty: true}
+      )
+        .pipe(
+          jsonMerge({
+            fileName: 'messages.json',
+            edit: (parsedJson, file) => {
+              if (isProduction) {
+                for (let [key, value] of Object.entries(parsedJson)) {
+                  if (value.hasOwnProperty('description')) {
+                    delete parsedJson[key].description;
+                  }
+                }
+              }
+              return parsedJson;
+            }
+          })
+        )
+        .pipe(gulpif(isProduction, jsonmin()))
+        .pipe(dest(path.join(distDir, '_locales', localeDir)))
+        .on('error', done)
+        .on('finish', resolve);
+    });
+  }
+}
+
+function manifest() {
+  return src('src/manifest.json')
+    .pipe(
+      jsonMerge({
+        fileName: 'manifest.json',
+        edit: (parsedJson, file) => {
+          if (['chrome', 'edge', 'opera'].includes(targetEnv)) {
+            delete parsedJson.browser_specific_settings;
+            delete parsedJson.options_ui.browser_style;
+          }
+
+          if (['chrome', 'edge', 'firefox'].includes(targetEnv)) {
+            delete parsedJson.minimum_opera_version;
+          }
+
+          if (['firefox', 'opera'].includes(targetEnv)) {
+            delete parsedJson.minimum_chrome_version;
+          }
+
+          if (targetEnv === 'firefox') {
+            delete parsedJson.options_ui.chrome_style;
+            delete parsedJson.incognito;
+          }
+
+          parsedJson.version = require('./package.json').version;
+          return parsedJson;
+        }
+      })
+    )
+    .pipe(gulpif(isProduction, jsonmin()))
+    .pipe(dest(distDir));
+}
+
+function license() {
+  let year = '2018';
+  const currentYear = new Date().getFullYear().toString();
+  if (year !== currentYear) {
+    year = `${year}-${currentYear}`;
+  }
+
+  const notice = `Buster: Captcha Solver for Humans
+Copyright (c) ${year} Armin Sebastian
+
+This software is released under the terms of the GNU General Public License v3.0.
+See the LICENSE file for further information.
+`;
+
+  writeFileSync(path.join(distDir, 'NOTICE'), notice);
+  return src(['LICENSE']).pipe(dest(distDir));
+}
+
+function secrets(done) {
+  try {
+    let data = process.env.BUSTER_SECRETS;
+    if (data) {
+      data = JSON.parse(data);
+    } else {
+      data = readJsonSync('secrets.json');
+    }
+    data = JSON.stringify(data);
+
+    const key = CryptoJS.SHA256(
+      readFileSync(path.join(distDir, 'src/background/script.js')).toString() +
+        readFileSync(path.join(distDir, 'src/solve/script.js')).toString()
+    ).toString();
+
+    const ciphertext = CryptoJS.AES.encrypt(data, key).toString();
+
+    writeFileSync(path.join(distDir, 'secrets.txt'), ciphertext);
+  } catch (err) {
+    console.log(
+      'Secrets are missing, secrets.txt will not be included in the extension package.'
+    );
+  }
+
+  done();
+}
+
+function zip(done) {
+  exec(
+    `web-ext build -s dist/${targetEnv} -a artifacts/${targetEnv} -n '{name}-{version}-${targetEnv}.zip' --overwrite-dest`,
+    function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      done(err);
+    }
+  );
+}
+
+function inspect(done) {
+  exec(
+    `webpack --profile --json > report.json && webpack-bundle-analyzer report.json dist/firefox/src && sleep 10 && rm report.{json,html}`,
+    function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      done(err);
+    }
+  );
+}
+
+exports.build = series(
+  clean,
+  parallel(js, html, css, images, fonts, locale, manifest, license),
+  secrets
+);
+exports.zip = zip;
+exports.inspect = inspect;
+exports.secrets = secrets;
---
+++ src/background/main.js
@@ -0,0 +1,488 @@
+import browser from 'webextension-polyfill';
+import audioBufferToWav from 'audiobuffer-to-wav';
+import aes from 'crypto-js/aes';
+import sha256 from 'crypto-js/sha256';
+import utf8 from 'crypto-js/enc-utf8';
+
+import {initStorage} from 'storage/init';
+import storage from 'storage/storage';
+import {
+  showNotification,
+  showContributePage,
+  sendNativeMessage
+} from 'utils/app';
+import {
+  executeCode,
+  executeFile,
+  scriptsAllowed,
+  functionInContext,
+  getBrowser,
+  getPlatform,
+  normalizeAudio,
+  sliceAudio
+} from 'utils/common';
+import {
+  recaptchaChallengeUrlRx} from 'utils/data';
+import {targetEnv, clientAppVersion} from 'utils/config';
+
+import { TranscribeClient, StartTranscriptionJobCommand, Transcribe } from "@aws-sdk/client-transcribe";
+import {S3, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3RequestPresigner } from"@aws-sdk/s3-request-presigner";
+import { createRequest } from"@aws-sdk/util-create-request";
+import { formatUrl } from"@aws-sdk/util-format-url";
+
+const config = {
+  region: String,
+  credentials: {
+    accessKeyId: String,
+    secretAccessKey: String
+  }
+};
+
+const transcribeClient = new TranscribeClient(config);
+const transcribeService = new Transcribe(config);
+const s3Client = new S3(config);
+
+let retryCount = 0;
+let nativePort;
+let secrets;
+
+function getFrameClientPos(index) {
+  let currentIndex = -1;
+  if (window !== window.top) {
+    const siblingWindows = window.parent.frames;
+    for (let i = 0; i < siblingWindows.length; i++) {
+      if (siblingWindows[i] === window) {
+        currentIndex = i;
+        break;
+      }
+    }
+  }
+
+  const targetWindow = window.frames[index];
+  for (const frame of document.querySelectorAll('iframe')) {
+    if (frame.contentWindow === targetWindow) {
+      let {left: x, top: y} = frame.getBoundingClientRect();
+      const scale = window.devicePixelRatio;
+
+      return {x: x * scale, y: y * scale, currentIndex};
+    }
+  }
+}
+
+async function getFramePos(tabId, frameId, frameIndex) {
+  let x = 0;
+  let y = 0;
+
+  while (true) {
+    frameId = (
+      await browser.webNavigation.getFrame({
+        tabId,
+        frameId
+      })
+    ).parentFrameId;
+    if (frameId === -1) {
+      break;
+    }
+
+    const [data] = await executeCode(
+      `(${getFrameClientPos.toString()})(${frameIndex})`,
+      tabId,
+      frameId
+    );
+
+    frameIndex = data.currentIndex;
+    x += data.x;
+    y += data.y;
+  }
+
+  return {x, y};
+}
+
+async function resetCaptcha(tabId, frameId, challengeUrl) {
+  frameId = (
+    await browser.webNavigation.getFrame({
+      tabId,
+      frameId: frameId
+    })
+  ).parentFrameId;
+
+  if (!(await scriptsAllowed(tabId, frameId))) {
+    await showNotification({messageId: 'error_scriptsNotAllowed'});
+    return;
+  }
+
+  if (!(await functionInContext('addListener', tabId, frameId))) {
+    await executeFile('/src/content/initReset.js', tabId, frameId);
+  }
+  await executeCode('addListener()', tabId, frameId);
+
+  await browser.tabs.sendMessage(
+    tabId,
+    {
+      id: 'resetCaptcha',
+      challengeUrl
+    },
+    {frameId}
+  );
+}
+
+function challengeRequestCallback(details) {
+  const url = new URL(details.url);
+  if (url.searchParams.get('hl') !== 'en') {
+    url.searchParams.set('hl', 'en');
+    return {redirectUrl: url.toString()};
+  }
+}
+
+async function setChallengeLocale() {
+  const {loadEnglishChallenge, simulateUserInput} = await storage.get(
+    ['loadEnglishChallenge', 'simulateUserInput'],
+    'sync'
+  );
+
+  if (loadEnglishChallenge || simulateUserInput) {
+    if (
+      !browser.webRequest.onBeforeRequest.hasListener(challengeRequestCallback)
+    ) {
+      browser.webRequest.onBeforeRequest.addListener(
+        challengeRequestCallback,
+        {
+          urls: [
+            'https://www.google.com/recaptcha/api2/anchor*',
+            'https://www.google.com/recaptcha/api2/bframe*',
+            'https://www.recaptcha.net/recaptcha/api2/anchor*',
+            'https://www.recaptcha.net/recaptcha/api2/bframe*',
+            'https://recaptcha.net/recaptcha/api2/anchor*',
+            'https://recaptcha.net/recaptcha/api2/bframe*',
+            'https://www.google.com/recaptcha/enterprise/anchor*',
+            'https://www.google.com/recaptcha/enterprise/bframe*',
+            'https://www.recaptcha.net/recaptcha/enterprise/anchor*',
+            'https://www.recaptcha.net/recaptcha/enterprise/bframe*',
+            'https://recaptcha.net/recaptcha/enterprise/anchor*',
+            'https://recaptcha.net/recaptcha/enterprise/bframe*'
+          ],
+          types: ['sub_frame']
+        },
+        ['blocking']
+      );
+    }
+  } else if (
+    browser.webRequest.onBeforeRequest.hasListener(challengeRequestCallback)
+  ) {
+    browser.webRequest.onBeforeRequest.removeListener(challengeRequestCallback);
+  }
+}
+
+function removeRequestOrigin(details) {
+  const origin = window.location.origin;
+  const headers = details.requestHeaders;
+  for (const header of headers) {
+    if (header.name.toLowerCase() === 'origin' && header.value === origin) {
+      headers.splice(headers.indexOf(header), 1);
+      break;
+    }
+  }
+
+  return {requestHeaders: headers};
+}
+
+function addBackgroundRequestListener() {
+  if (
+    !browser.webRequest.onBeforeSendHeaders.hasListener(removeRequestOrigin)
+  ) {
+    const urls = [
+      'https://www.google.com/*',
+      'https://www.recaptcha.net/*',
+      'https://recaptcha.net/*',
+    ];
+
+    const extraInfo = ['blocking', 'requestHeaders'];
+    if (
+      targetEnv !== 'firefox' &&
+      Object.values(browser.webRequest.OnBeforeSendHeadersOptions).includes(
+        'extraHeaders'
+      )
+    ) {
+      extraInfo.push('extraHeaders');
+    }
+
+    browser.webRequest.onBeforeSendHeaders.addListener(
+      removeRequestOrigin,
+      {
+        urls,
+        types: ['xmlhttprequest']
+      },
+      extraInfo
+    );
+  }
+}
+
+function removeBackgroundRequestListener() {
+  if (browser.webRequest.onBeforeSendHeaders.hasListener(removeRequestOrigin)) {
+    browser.webRequest.onBeforeSendHeaders.removeListener(removeRequestOrigin);
+  }
+}
+
+async function prepareAudio(audio) {
+  const audioBuffer = await normalizeAudio(audio);
+
+  const audioSlice = await sliceAudio({
+    audioBuffer,
+    start: 1.5,
+    end: audioBuffer.duration - 1.5
+  });
+
+  return audioBufferToWav(audioSlice);
+}
+
+async function loadSecrets() {
+  try {
+    const ciphertext = await (await fetch('/secrets.txt')).text();
+
+    const key = sha256(
+      (await (await fetch('/src/background/script.js')).text()) +
+        (await (await fetch('/src/solve/script.js')).text())
+    ).toString();
+
+    secrets = JSON.parse(aes.decrypt(ciphertext, key).toString(utf8));
+  } catch (err) {
+    secrets = {};
+    const {speechService} = await storage.get('speechService', 'sync');
+    if (speechService === 'witSpeechApiDemo') {
+      await storage.set({speechService: 'witSpeechApi'}, 'sync');
+    }
+  }
+}
+
+async function getAwsTrascribeApiResult(audioContent) {
+
+  const key = `captcha-transcribe-job-${Math.floor(Date.now() / 1000)}`;
+  const signedUrl = await createPresignedUrl(key);
+  const response = await putAudioInBucket(signedUrl, audioContent);
+
+  const command = new StartTranscriptionJobCommand({
+    TranscriptionJobName: key,
+    // The language code for the input audio. Valid values are en-GB, en-US, es-US, fr-CA, and fr-FR.
+    LanguageCode: "en-US",
+    // The encoding used for the input audio. The only valid value is pcm.
+    Media: {
+      MediaFileUri:  "s3://demo-awstranscribe/" + key + '.wav',
+      // For example, "https://transcribe-demo.s3-REGION.amazonaws.com/hello_world.wav"
+    }
+  });
+
+  const JobResult = await transcribeClient.send(command);
+  await sleep(10000);
+  retryCount = 0;
+  const solution = await getTranscriptWithRetry(command.input.TranscriptionJobName);
+  return solution;
+}
+
+async function getTranscriptWithRetry(jobName) {
+  const result = await transcribeService.getTranscriptionJob({ TranscriptionJobName: jobName });
+
+  if (result.TranscriptionJob.TranscriptionJobStatus === 'COMPLETED') {
+    if (result.TranscriptionJob?.Transcript?.TranscriptFileUri) {
+      const jsonResult = await (await fetch(result.TranscriptionJob?.Transcript?.TranscriptFileUri)).json();
+      console.log(jsonResult);
+      return jsonResult.results.transcripts[0].transcript;
+    }
+  } else {
+    if (retryCount <= 3) {
+      retryCount++;
+      await sleep(5000);
+      return await getTranscriptWithRetry(jobName);
+    } else {
+      console.log('Retry exceeded...');
+      showNotification({ messageId: 'error_internalError' });
+      return null;
+    }
+  }
+}
+
+async function createPresignedUrl(key){
+  // Create a presigned URL to upload the transcription to the Amazon S3 bucket when it is ready.
+  try {
+    // Create an Amazon S3RequestPresigner object.
+    const signer = new S3RequestPresigner({...s3Client.config});
+    // Create the request.
+    const request = await createRequest(
+      s3Client,
+      new PutObjectCommand({ Key: key + '.wav', Bucket: 'demo-awstranscribe' })
+    );
+    // Define the duration until expiration of the presigned URL.
+    const expiration = new Date(Date.now() + 60 * 60 * 1000);
+    // Create and format the presigned URL.
+    let signedUrl;
+    signedUrl = formatUrl(await signer.presign(request, expiration));
+    console.log(`\nPutting "${key}"`);
+    return signedUrl;
+  } catch (err) {
+    console.log("Error creating presigned URL", err);
+  }
+}
+
+async function putAudioInBucket(signedUrl, audioContent) {
+  let response;
+  try {
+    response = await fetch(signedUrl, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/octet-stream'
+      },
+      body: new Blob([audioContent], {type: 'audio/wav'})
+    });
+  } catch (e) {
+    console.log('Error in putAudioInBucket', err);
+  }
+
+  return response;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function transcribeAudio(audioUrl, lang) {
+  let solution;
+
+  const audioRsp = await fetch(audioUrl, {referrer: ''});
+  const audioContent = await prepareAudio(await audioRsp.arrayBuffer());
+
+  if(!secrets){
+    await loadSecrets();
+  }
+
+  solution = await getAwsTrascribeApiResult(audioContent);
+
+  if (!solution) {
+    showNotification({messageId: 'error_captchaNotSolved', timeout: 6000});
+  } else {
+    return solution;
+  }
+}
+
+async function onMessage(request, sender) {
+  if (request.id === 'notification') {
+    showNotification({
+      message: request.message,
+      messageId: request.messageId,
+      title: request.title,
+      type: request.type,
+      timeout: request.timeout
+    });
+  } else if (request.id === 'captchaSolved') {
+    let {useCount} = await storage.get('useCount', 'sync');
+    useCount += 1;
+    await storage.set({useCount}, 'sync');
+    if ([30, 100].includes(useCount)) {
+      await showContributePage('use');
+    }
+  } else if (request.id === 'transcribeAudio') {
+    addBackgroundRequestListener();
+    try {
+      return await transcribeAudio(request.audioUrl, request.lang);
+    } finally {
+      removeBackgroundRequestListener();
+    }
+  } else if (request.id === 'resetCaptcha') {
+    await resetCaptcha(sender.tab.id, sender.frameId, request.challengeUrl);
+  } else if (request.id === 'getFramePos') {
+    return getFramePos(sender.tab.id, sender.frameId, request.frameIndex);
+  } else if (request.id === 'getTabZoom') {
+    return browser.tabs.getZoom(sender.tab.id);
+  } else if (request.id === 'getBackgroundScriptScale') {
+    return window.devicePixelRatio;
+  } else if (request.id === 'startClientApp') {
+    nativePort = browser.runtime.connectNative('org.buster.client');
+  } else if (request.id === 'stopClientApp') {
+    if (nativePort) {
+      nativePort.disconnect();
+    }
+  } else if (request.id === 'messageClientApp') {
+    const message = {
+      apiVersion: clientAppVersion,
+      ...request.message
+    };
+    return sendNativeMessage(nativePort, message);
+  } else if (request.id === 'openOptions') {
+    browser.runtime.openOptionsPage();
+  } else if (request.id === 'getPlatform') {
+    return getPlatform();
+  } else if (request.id === 'getBrowser') {
+    return getBrowser();
+  }
+}
+
+async function onStorageChange(changes, area) {
+  await setChallengeLocale();
+}
+
+function addStorageListener() {
+  browser.storage.onChanged.addListener(onStorageChange);
+}
+
+function addMessageListener() {
+  browser.runtime.onMessage.addListener(onMessage);
+}
+
+async function onInstall(details) {
+  if (
+    ['chrome', 'edge', 'opera'].includes(targetEnv) &&
+    ['install', 'update'].includes(details.reason)
+  ) {
+    const tabs = await browser.tabs.query({
+      url: ['http://*/*', 'https://*/*'],
+      windowType: 'normal'
+    });
+
+    for (const tab of tabs) {
+      const tabId = tab.id;
+
+      const frames = await browser.webNavigation.getAllFrames({tabId});
+      for (const frame of frames) {
+        const frameId = frame.frameId;
+
+        if (frameId && recaptchaChallengeUrlRx.test(frame.url)) {
+          await browser.tabs.insertCSS(tabId, {
+            frameId,
+            runAt: 'document_idle',
+            file: '/src/solve/reset-button.css'
+          });
+
+          await browser.tabs.executeScript(tabId, {
+            frameId,
+            runAt: 'document_idle',
+            file: '/src/manifest.js'
+          });
+          await browser.tabs.executeScript(tabId, {
+            frameId,
+            runAt: 'document_idle',
+            file: '/src/solve/script.js'
+          });
+        }
+      }
+    }
+
+    const setupTabs = await browser.tabs.query({
+      url: 'http://127.0.0.1/buster/setup?session=*',
+      windowType: 'normal'
+    });
+
+    for (const tab of setupTabs) {
+      await browser.tabs.reload(tab.id);
+    }
+  }
+}
+
+async function onLoad() {
+  await initStorage('sync');
+  await setChallengeLocale();
+  addStorageListener();
+  addMessageListener();
+}
+
+browser.runtime.onInstalled.addListener(onInstall);
+
+document.addEventListener('DOMContentLoaded', onLoad);
